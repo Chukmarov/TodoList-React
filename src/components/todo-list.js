01cfcba5638720aacd25f14a5ae11825c3b63926
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-12-01 23:42:24 +0500

    Добавил css, Bootstrap и FontAwesome

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './todo-list.css';
 
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+    // ... - это spread оператор для объекта. Он переписывает значения из объектов
+    // id - это reconcilation (примирение, соласие) algorithm, для ускорения поиска изменений и обновления только этих алгоритмов
+    const elements = todos.map((item) =>{
+        const { id, ...itemProps } = item; // вот это синтаксис деструктуризации. Мы разложили item на константу и другой объект
+        return ( 
+            <li key={id} className="list-group-item">
+                <TodoListItem {...itemProps} /> 
+            </li>
+        )
+    });
+
     return(
-        <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+        <ul className ='list-group todo-list'>
+            {elements}
         </ul>
     )
 }
