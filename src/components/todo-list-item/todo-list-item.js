d70ae5aeb87174962b2c85d47c1bbdb2111ca7db
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-12-06 13:27:47 +0500

    добавил объект состояния и функцию setState для изменения объекта состояния. прописал функицю зачеркивания выполненных дел и функцию пометки дел как важные

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,25 +2,54 @@ import React, { Component } from 'react';
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component{
+
+
+  //ниже объект состояния элемента. Его нельзя перезаписывать. Изменить объект state можно только функцией setState
+  state ={
+    done: false,
+    important: false
+  };
+
+  // применение функции setState
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    })
+  };
+
+  onMarkImportant = () =>{
+    this.setState({
+      important:true
+    })
+  }
+
   render(){
 
-    const {label, important = false} = this.props;
+    const {label} = this.props;
+    // достаем значение done из state
+    const {done, important} = this.state;
+    // создаем переменную чтоб дальше исползовать в функции
+    let classNames = 'todo-list-item';
+    // проверяем состояние done , если так как нам нужно то перезаписываем , добавляя еще один класс. Эти классы подгружаеются автоматически из BootStrap
+    if (done){
+      classNames += ' done';
+    }
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
-  
+    if (important){
+      classNames += ' important';
+    }
+
      return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}>
+          onClick ={this.onLabelClick}>
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick ={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
