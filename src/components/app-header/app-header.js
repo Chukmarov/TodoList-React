efdf7cba0839da9268ee74743f24a64746bd7b30
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-12-07 23:27:01 +0500

    добавил форму и механизм добавления новых дел из этой формы

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ const AppHeader = ({todo, done}) => {
     return(
       <div className = 'app-header d-flex'>
         <h1>My Todo List</h1>
-        <h2>{todo} еще предстоит сделать, {done} сделано</h2>
+        <h2>{todo} впереди, {done} сделано</h2>
       </div>
     ); 
   };
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,14 +3,40 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state ={
+        label : ''
+    };
+    // эта функция фиксирует изменения происходящие в label и записывает изменения в state
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+    // функция сабмита, которая запускает функицю отрисовки формы
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label:''
+        });
+    };
+
     render(){
         return(
-            <div className ="item-add-form">
-                <button className = 'btn btn-outline-secondary'
-                onClick={ () => this.props.onItemAdded('Hello World') }>
-                    Add Item
-                </button>
-            </div>
+            <form className ="item-add-form d-flex"
+                // обрати внимание как в реакте записывается функция Submit
+                    onSubmit= {this.onSubmit} >
+                <input type ='text'
+                        className='form-control'
+                        onChange = {this.onLabelChange}
+                        placeholder='Что нужно сделать'
+                        //важный момент заключается в этом пункте. Это называется контролируемый компонент. Т.е. label и значение value связываются вместе
+                        value = {this.state.label}
+                        />
+                 <button className = 'btn btn-outline-secondary add-button'>
+                Add Item</button>
+            </form>
         )
     }
 }
